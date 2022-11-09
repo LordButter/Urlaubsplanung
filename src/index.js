a730009e90c245dcf0a72e0d0a84010892dbb024
Bastian Hollasch <bastian.hollasch@bbs-meppen.de>
2022-11-09 11:55:11 +0100

    remove menubar and bugfixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
+        autoHideMenuBar: true,
         width: 800,
-        height: 600
-    })
-  
-    win.loadFile('index.html')
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    win.loadFile('index.html');
 }
 
 app.commandLine.appendSwitch('no-sandbox');
 app.commandLine.appendSwitch('in-process-gpu');
 app.whenReady().then(() => {
     createWindow()
-})
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    })
+});
