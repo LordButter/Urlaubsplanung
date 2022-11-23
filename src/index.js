f9d502a3b16f6c0aefe2c457d470c6ae6d6116fc
BHollasch <bhollasch@wocken-it.com>
2022-11-23 08:53:51 +0100

    hinzufügen von tailwind

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const path = require('path')
 const createWindow = () => {
     const win = new BrowserWindow({
         autoHideMenuBar: true,
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
